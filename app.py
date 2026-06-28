07057006ebf4ef2f88a3f5a6718005a50cf6fa0b
Rangam Mishra <rangammishra@gmail.com>
2026-06-28 02:07:25 -0700

    Test automatic deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Rangam! App deployed using GitHub Actions and AWS EC2."
+    return "return "🚀 Automatic deployment is working! CI/CD Success!""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
